671df2491a0f1aa0632403d9eeeac1e514f5f72c
NikoFresh <nicolo.frescura@pm.me>
2021-04-14 17:03:56 +0200

    Add a config file

---
+++ config.py
@@ -0,0 +1,2 @@
+class Config():
+    RSS_URL: str = 'https://www.repubblica.it/rss/cronaca/rss2.0.xml'
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import difflib
 import asyncio
 
 from src.parser import parse
-
-RSS_URL: str = 'https://www.repubblica.it/rss/cronaca/rss2.0.xml'
-
-
+from config import Config
 
 async def main() -> None:
     while True:
-        parse(RSS_URL)
+        parse(Config.RSS_URL)
         await asyncio.sleep(5)
-    
 
 if __name__ == '__main__':
     asyncio.run(main())
