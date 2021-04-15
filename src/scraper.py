c615fe3c0961dbfefa0ad31976139cc90091f74d
NikoFresh <nicolo.frescura@pm.me>
2021-04-15 15:16:40 +0200

    .text -> .get_text(strip=True)

--- src/scraper.py
+++ src/scraper.py
@@ -10,8 +10,8 @@ def scrape_article(link: str) -> str:
     # The article contains some link to other news inside <section> tags. Remove them
     [tag.decompose() for tag in soup.find_all('section', attrs={'class':'inline-article'})]
 
-    title: str = soup.find('h1', attrs={'class':'story__title'}).text
-    summary: str = soup.find('div', attrs={'class':'story__summary'}).text
-    content: str = soup.find('div', attrs={'class':'story__text'}).text
+    title: str = soup.find('h1', attrs={'class':'story__title'}).get_text(strip=True)
+    summary: str = soup.find('div', attrs={'class':'story__summary'}).get_text(strip=True)
+    content: str = soup.find('div', attrs={'class':'story__text'}).get_text(strip=True)
 
     return title, summary, content
