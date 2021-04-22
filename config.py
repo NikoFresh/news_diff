8169fad6abae62cf89649a037f487a47f9913a13
NikoFresh <nicolo.frescura@pm.me>
2021-04-22 15:48:04 +0200

    Load some config from .env

--- config.py
+++ config.py
@@ -1,2 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+
 class Config:
-    RSS_URL: str = "https://www.repubblica.it/rss/cronaca/rss2.0.xml"
+    load_dotenv(".env")
+
+    TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
+    CHAT_ID: int = os.getenv("TELEGRAM_ID")
+    RSS_URL: str = "http://xml2.corriereobjects.it/rss/homepage.xml"
+    HTML_TEMPLATE: str = """
+        <html>
+          <head>
+           
+          </head>
+          <body>
+            <p>
+                {}
+            </p>
+          </body>
+        </html>
+    """
