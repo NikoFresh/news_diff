828c6fe81038160ca6594aa4cdb795ae18b403aa
NikoFresh <nicolo.frescura@pm.me>
2021-04-24 19:38:10 +0200

    environ -> getenv

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     load_dotenv(".env")
 
     sleep_time: int = 300
-    DB_URL: str = os.environ("DATABASE_URL")
+    DB_URL: str = os.getenv("DATABASE_URL")
     TELEGRAM_TOKEN: str = os.getenv("TELEGRAM_TOKEN")
     CHAT_ID: int = os.getenv("TELEGRAM_ID")
     RSS_URL: str = "http://xml2.corriereobjects.it/rss/homepage.xml"
